3d40d86c43a64b1f6334900ee24bef48f8f6568f
Dalia Garcia <dalia.garcia16@hotmail.com>
2020-07-04 13:11:23 -0400

    menu listo

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -1,11 +1,55 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink, withRouter}  from 'react-router-dom';
+import firebase from '../firebaseConfig/firebase.js';
+//import LogoHeader from './LogoHeader.jsx';
+import logoHeader from '../assets/images/logo1W.png';
+import '../assets/css/HeaderMenu.css';
 
-const HeaderMenu = () => {
+
+
+
+class Navbar extends React.Component {
+ 
+    getNavLinkClass = (path) => {
+        return this.props.location.pathname === path ? 'active' : '';
+    }
+
+     logOut = () => {
+      firebase.auth().signOut();
+   
+  
+      }
+ 
+    render() {
+        return (
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <img src={logoHeader} className="Logo-Header" alt="logo" />
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav mr-auto">
+
+            <li className={this.getNavLinkClass("/home")}><NavLink to="/home" >Inicio</NavLink></li>
+                <li className={this.getNavLinkClass("/menu")}><NavLink to="/menu">Menu</NavLink></li>
+                <li className={this.getNavLinkClass("/orderchef")}><NavLink to="/orderchef">Pedidos chef</NavLink></li>
+                <li className={this.getNavLinkClass("/orderwaiter")}><NavLink to="/orderwaiter">Pedidos mesero</NavLink></li>
+              
+            </ul>
+            
+            <span className={this.getNavLinkClass("/login")} onClick={()=> this.logOut("/login")}><NavLink to="/login">Cerrar sesion</NavLink></span>
+            </div>
+          
+        </nav>
+        )
+    }
+};
+Navbar = withRouter(Navbar);
+export default Navbar;
+/*const HeaderMenu = () => {
 
 return(
-    
-        
+          
           <div>
             <nav>
               <ul>
@@ -33,4 +77,4 @@ return(
 
 }
 
-export default HeaderMenu;
+export default HeaderMenu;*/
