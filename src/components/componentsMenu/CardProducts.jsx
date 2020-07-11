cac8652916d6b4a33c3c9d632ebb6e872f7b5567
EliHidalgo <hidalgo_eliana@hotmail.com>
2020-07-11 13:47:26 -0400

    trabajando en orden de pedido para pasarlo a boleta

--- src/components/componentsMenu/CardProducts.jsx
+++ src/components/componentsMenu/CardProducts.jsx
@@ -3,21 +3,19 @@ import '../componentsMenu/CardProducts.css';
 
 const ViewCardProduct = (props) => {
 
-    return (
 
+    return (
      
 
-          <div className="card" style={{width: '10rem', height: 'auto'}}>
-            <div className = "img-Container">
-            <img src={props.img} className="card-img-top" alt="..."/> 
-            </div>
- 
-              <div className="card-body" style={{padding: '15px'}}>
-                <h6 className="card-title">{props.name}</h6>
+          <button className="card" style={{width: '10rem', height: 'auto'}} onClick = {props.onClick}>
+            <div className="card-body">
+                {/* <h6 className="card-title">{props.name}</h6>
                 <h6 className="card-text">$ {props.price}</h6>
-                <a href='/' onClick={props.onClick} className="btn btn-primary">Agregar</a>
-              </div>
-          </div>
+                <a href='/' onClick={props.onClick} className="btn btn-primary">Agregar</a> */}
+                <img src={props.img} className="card-img-top" alt="..."/> 
+            </div>
+              <div className="card-footer" > <p>{props.name}</p> <p>$ {props.price}</p> </div>
+          </button>
 
       
 
